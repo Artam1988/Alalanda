e2654da21511da9fe657f2cf11f829403ad3cd73
hosam <semryhosam@gmail.com>
2025-06-18 13:44:57 +0300

    Refactored settings and requirements: update DEBUG setting, add CORS middleware, and clean up dependencies

--- project/settings.py
+++ project/settings.py
@@ -14,10 +14,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure--v2xib3q4)ys-2$rz0b47in^8609!9d#n^xd!31j#!@n1#bkay')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure--v   2xib3q4)ys-2$rz0b47in^8609!9d#n^xd!31j#!@n1#bkay')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', 'True').lower() == 'true'
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', '.onrender.com']
 
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'parler',
     'products',
     'core',
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -46,6 +48,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+]
+
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    'https://alalanda.onrender.com',
 ]
 
 PARLER_LANGUAGES = {
